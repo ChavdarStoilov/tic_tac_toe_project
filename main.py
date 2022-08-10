56bd6091fcf27b1e5ba928b7ef2091335903d124
Chavdar Velichkov Stoilov <stoilov.chavdar@gmail.com>
2022-08-10 17:17:29 +0300

    Correction on script
    new version of script

--- main.py
+++ main.py
@@ -1,169 +1,170 @@
 import random
 
+places = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
 
-def board(place):
+def board():
     print("------------------------\n       |       |")
-    print(f"   {place.get(1)}   |   {place.get(2)}   |   {place.get(3)}   \n       |       |       ")
+    print(f"   {places.get(1)}   |   {places.get(2)}   |   {places.get(3)}   \n       |       |       ")
     print("------------------------\n       |       |")
-    print(f"   {place.get(4)}   |   {place.get(5)}   |   {place.get(6)}   \n       |       |       ")
+    print(f"   {places.get(4)}   |   {places.get(5)}   |   {places.get(6)}   \n       |       |       ")
     print("------------------------\n       |       |")
-    print(f"   {place.get(7)}   |   {place.get(8)}   |   {place.get(9)}   \n       |       |       ")
+    print(f"   {places.get(7)}   |   {places.get(8)}   |   {places.get(9)}   \n       |       |       ")
     print("------------------------")
 
-def winner(place):
+def winner():
 
-    if place[1] == "x" and place[2] == "x" and place[3] == "x":
+    if places[1] == "x" and places[2] == "x" and places[3] == "x":
         return "Computer win"
-    elif place[1] == "o" and place[2] == "o" and place[3] == "o":
+    elif places[1] == "o" and places[2] == "o" and places[3] == "o":
         return "You win"
 
-    if place[4] == "x" and place[5] == "x" and place[6] == "x":
+    if places[4] == "x" and places[5] == "x" and places[6] == "x":
         return "Computer win"
-    elif place[4] == "o" and place[5] == "o" and place[6] == "o":
+    elif places[4] == "o" and places[5] == "o" and places[6] == "o":
         return "You win"
 
-    if place[7] == "x" and place[8] == "x" and place[9] == "x":
+    if places[7] == "x" and places[8] == "x" and places[9] == "x":
         return "Computer win"
-    elif place[7] == "o" and place[8] == "o" and place[9] == "o":
+    elif places[7] == "o" and places[8] == "o" and places[9] == "o":
         return "You win"
 
-    if place[1] == "x" and place[4] == "x" and place[7] == "x":
+    if places[1] == "x" and places[4] == "x" and places[7] == "x":
         return "Computer win"
-    elif place[1] == "o" and place[4] == "o" and place[7] == "o":
+    elif places[1] == "o" and places[4] == "o" and places[7] == "o":
         return "You win"
 
-    if place[2] == "x" and place[5] == "x" and place[8] == "x":
+    if places[2] == "x" and places[5] == "x" and places[8] == "x":
         return "Computer win"
-    elif place[2] == "o" and place[5] == "o" and place[8] == "o":
+    elif places[2] == "o" and places[5] == "o" and places[8] == "o":
         return "You win"
 
-    if place[3] == "x" and place[6] == "x" and place[9] == "x":
+    if places[3] == "x" and places[6] == "x" and places[9] == "x":
         return "Computer win"
-    elif place[3] == "o" and place[6] == "o" and place[9] == "o":
+    elif places[3] == "o" and places[6] == "o" and places[9] == "o":
         return "You win"
 
-    if place[1] == "x" and place[5] == "x" and place[9] == "x":
+    if places[1] == "x" and places[5] == "x" and places[9] == "x":
         return "Computer win"
-    elif place[1] == "o" and place[5] == "o" and place[9] == "o":
+    elif places[1] == "o" and places[5] == "o" and places[9] == "o":
         return "You win"
 
-    if place[7] == "x" and place[5] == "x" and place[3] == "x":
+    if places[7] == "x" and places[5] == "x" and places[3] == "x":
         return "Computer win"
-    elif place[7] == "o" and place[5] == "o" and place[3] == "o":
+    elif places[7] == "o" and places[5] == "o" and places[3] == "o":
         return "You win"
 
 
 
-def choose(computer_choice, place):
+def choose(computer_choice):
 
-    if place[computer_choice] == " ":
-        place[computer_choice] = "x"
+    if places[computer_choice] == " ":
+        places[computer_choice] = "x"
     else:
-        computer_choosing(place)
+        computer_choosing()
 
 
 
-def computer_choosing(place):
+def computer_choosing():
 
 
     computer = random.randrange(1, 9)
 
-    choose(computer, place)
+    choose(computer)
 
-def new_computer_choose(place):
+def new_computer_choose():
 
-    if place[9] == "o" and place[5] == "o" or place[4] and place[7] == "o" or place[3] == "o" and place[2] == "o":
-        if place[1] == " ":
-            place[1] = "x"
+    if places[9] == "o" and places[5] == "o" or places[4] == "o" and places[7] == "o" or places[3] == "o" and places[2] == "o":
+        if places[1] == " ":
+            places[1] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[1] == "о" and place[3] == "o" or place[8] == "o" and place[5] == "o":
-        if place[2] == " ":
-            place[2] = "x"
+    elif places[1] == "о" and places[3] == "o" or places[8] == "o" and places[5] == "o":
+        if places[2] == " ":
+            places[2] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[7] == "o" and place[5] == "o" or place[1] == "o" and place[2] == "o" or place[9] == "o" and place[6] == "o":
-        if place[3] == " ":
-            place[3] = "x"
+    elif places[7] == "o" and places[5] == "o" or places[1] == "o" and places[2] == "o" or places[9] == "o" and places[6] == "o":
+        if places[3] == " ":
+            places[3] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[6] == "o" and place[5] == "o" or place[1] == "o" and place[7] == "o":
-        if place[4] == " ":
-            place[4] = "x"
+    elif places[6] == "o" and places[5] == "o" or places[1] == "o" and places[7] == "o":
+        if places[4] == " ":
+            places[4] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[9] == "o" and place[1] == "o" or place[7] == "o" and place[3] == "o" or place[4] == "о" and place[6] == "o" or place[2] == "о" and place[8] == "o":
-        if place[5] == " ":
-            place[5] = "x"
+    elif places[9] == "o" and places[1] == "o" or places[7] == "o" and places[3] == "o" or places[4] == "о" and places[6] == "o" or places[2] == "о" and places[8] == "o":
+        if places[5] == " ":
+            places[5] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[4] == "o" and place[5] == "o" or place[3] == "0" and place[9] == "o":
-        if place[6] == " ":
-            place[6] = "x"
+    elif places[4] == "o" and places[5] == "o" or places[3] == "0" and places[9] == "o":
+        if places[6] == " ":
+            places[6] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[3] == "o" and place[5] == "o" or place[8] == "0" and place[9] == "o" or place[1] == "o" and place[4] == "o":
-        if place[7] == " ":
-            place[7] = "x"
+    elif places[3] == "o" and places[5] == "o" or places[8] == "0" and places[9] == "o" or places[1] == "o" and places[4] == "o":
+        if places[7] == " ":
+            places[7] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[9] == "o" and place[7] == "o" or place[2] == "o" and place[5] == "o":
-        if place[8] == " ":
-            place[8] = "x"
+    elif places[9] == "o" and places[7] == "o" or places[2] == "o" and places[5] == "o":
+        if places[8] == " ":
+            places[8] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
-    elif place[1] == "o" and place[5] == "o" or place[7] == "o" and place[8] == "o" or place[3] == "o" and place[6] == "o":
-        if place[9] == " ":
-            place[9] = "x"
+    elif places[1] == "o" and places[5] == "o" or places[7] == "o" and places[8] == "o" or places[3] == "o" and places[6] == "o":
+        if places[9] == " ":
+            places[9] = "x"
         else:
-            computer_choosing(place)
+            computer_choosing()
 
     else:
-        computer_choosing(place)
+        computer_choosing()
 
-    board(place)
+    board()
 
-def your_choosing(choose, place):
+def your_choosing(choose):
     your_choose = int(choose)
-    if place[int(your_choose)] == " ":
-        place[int(your_choose)] = "o"
+    if places[int(your_choose)] == " ":
+        places[int(your_choose)] = "o"
 
 
-    if winner(place) == "Computer win" or winner(place) == "You win":
-        board(place)
-        print(winner(place))
+    if winner() == "Computer win" or winner() == "You win":
+        board()
+        print(winner())
         exit()
 
-    elif place[1] == " " or place[2] == " " or place[3] == " " or place[4] == " " or place[5] == " " or \
-            place[6] == " " or place[7] == "" or place[8] == " " or place[9] == " ":
+    elif places[1] == " " or places[2] == " " or places[3] == " " or places[4] == " " or places[5] == " " or \
+            places[6] == " " or places[7] == "" or places[8] == " " or places[9] == " ":
 
         print("Computer choosing....")
-        new_computer_choose(place)
+        new_computer_choose()
 
     else:
         print("There no winner")
         exit()
 
-places = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
-board(places)
+
+board()
 start = input("Choose number from 1 to 9: ")
 
 while start != "stop":
     if start == "":
         start = input("Cannot enter empty input, please enter number from 1 to 9:")
 
-    your_choosing(start, places)
+    your_choosing(start)
 
-    if winner(places) == "Computer win" or winner(places) == "You win":
-        print(winner(places))
+    if winner() == "Computer win" or winner() == "You win":
+        print(winner())
         break
 
     start = input("Choose number from 1 to 9: ")
